5f9c042b48ff9f60830b25d3590ee2b8a1ebe713
root <hjguitgz@tgz.zgf>
2025-09-01 13:40:28 +0000

    Fetch data logic added for `messwerte` and `pflanze` tables in `database.py`.

--- utils/database.py
+++ utils/database.py
@@ -1,35 +1,62 @@
 # db connection
 
-
-import sqlite3
-import pymysql
 import pymysql.cursors
 
 
 # connect to database
 try:
-    connection = pymysql.connect(host="192.168.189.6",
-                                 user="anakin",
-                                 password="anakin2025!",
-                                 db="flanzenbewaesserung",
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
+    auth = pymysql.connect(host="192.168.189.6",
+                           user="anakin",
+                           password="anakin2025!",
+                           db="flanzenbewaesserung",
+                           charset='utf8mb4',
+                           cursorclass=pymysql.cursors.DictCursor)
 except:
-    print("Connection Error")
+    print('Can´t connect to database', auth)
 
-'''''
-with connection:
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO `role` (`id`, `name`, `type`, `value`) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+# create cursor
+cursor = auth.cursor()
 
-    # commit to save changes
-    connection.commit()
+# read from a database "messwerte"
+sql_query = ("SELECT * FROM messwerte")
 
-    with connection.cursor() as cursor:
-        # read a single record
-        sql = "SELECT * FROM `role`"
-        cursor.execute(sql, (1, "test", "temperature", 20.0))
-'''
+# execute the query
+try:
+    cursor.execute(sql_query)
+    result = cursor.fetchall()
+
+    for record in result:
+        ID = record[0]
+        PID = record[1]
+        Zeit = record[2]
+        Humidity = record[3]
+        Light = record[4]
+        Temperature = record[5]
+        Air = record[6]
+
+        print(ID, PID, Zeit, Humidity, Light, Temperature, Air)
+
+except Exception as e:
+    print("Exception Occurred", e)
+
+cursor.close()
+
+# read from a database "pflanze"
+sql_query = ("SELECT * FROM pflanze")
+
+# execute the query
+try:
+    cursor.execute(sql_query)
+    result = cursor.fetchall()
+
+    for record in result:
+        ID = record[0]
+        Schwellwert = record[1]
+        Name = record[2]
+
+        print(ID, Schwellwert, Name)
+
+except Exception as e:
+    print("Exception Occurred", e)
+
+cursor.close()
